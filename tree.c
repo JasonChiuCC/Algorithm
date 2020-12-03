576fb4fa760c3ca9e8c51317e50869caa516618d
JasonChiuCC <jasonchiucc@gmail.com>
2020-12-03 11:21:51 +0800

    Update tree.c

--- tree.c
+++ tree.c
@@ -1,24 +1,44 @@
 
-
+/*
+home
+ -sub1
+   -A
+   -B
+ -sub2
+   -C
+   -D
+*/
 struct DIR {
 	char name[256];
 }
 
-struct DIR* opendir(char* name){...}
-struct DIR* readir(struct DIR* dir){...}
+// return file/DIR
+struct DIR* opendir(char* name)     {...}
 
+// return file/DIR under input
+struct DIR* readir (struct DIR* dir){...}
 
+// Example : input = home
 void tree(char* name)
 {
 	struct DIR *pDir_Curr;
-	struct DIR *pDir_Next;
+	struct DIR *pDir_Entry;
 	
+	// Check
 	if( opendir(name) == NULL ) return;
 	
-	pDir_Curr = opendir(name);
-	printf("%s\n", pDir_Curr->name );
-
-	pDir_Next = readir(pDir_Curr);
-	tree( pDir_Next->name );
+	// Loop Current folder
+	while( (pDir_Entry = readir(pDir_Curr)) != NULL )
+	{
+        if ( strcmp(entry->d_name, ".")  == 0 || 
+             strcmp(entry->d_name, "..") == 0)
+        {
+            continue;
+        }
+   
+        // Display Current folder name
+        printf(" %s \n", pDir_Entry->name );
+        tree( pDir_Entry->name );
+	}
 	return;
 }
